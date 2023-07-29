fda25669b88a0190315a99bbec107dfcbf30c08b
Abogeerick <abogeerick@gmail.com>
2023-07-29 20:03:03 +0300

    Added delete button that deletes the bots both frontend and backend

--- src/components/YourBotArmy.jsx
+++ src/components/YourBotArmy.jsx
@@ -3,6 +3,30 @@ import { Link } from "react-router-dom";
 import "../styles.css";
 
 function YourBotArmy({ army, releaseBot }) {
+  const handleReleaseBot = (botId) => {
+    // Call the releaseBot function passed from the parent component (App) to release the bot from the army
+    releaseBot(botId);
+  };
+
+  const handleDeleteBot = (botId) => {
+    // Call the releaseBot function passed from the parent component (App) to delete the bot
+    releaseBot(botId); // This will release the bot from the army (frontend)
+    // Now, make an API call to delete the bot from the backend
+    fetch(`http://localhost:3000/bots/${botId}`, {
+      method: "DELETE",
+    })
+      .then((response) => {
+        if (response.ok) {
+          console.log("Bot deleted successfully from backend.");
+        } else {
+          throw new Error("Failed to delete bot from backend.");
+        }
+      })
+      .catch((error) => {
+        console.error("Error deleting bot from backend: ", error);
+      });
+  };
+
   return (
     <div>
       <h2>Your Bot Army</h2>
@@ -10,7 +34,13 @@ function YourBotArmy({ army, releaseBot }) {
         {army.map((bot) => (
           <li key={bot.id}>
             {bot.name} - {bot.type}
-            <button onClick={() => releaseBot(bot.id)}>Release</button>
+            <button onClick={() => handleReleaseBot(bot.id)}>Release</button>
+            <button
+              onClick={() => handleDeleteBot(bot.id)}
+              style={{ color: "red" }}
+            >
+              X
+            </button>
             <img src={bot.avatar_url} alt={`Avatar of ${bot.name}`} />
           </li>
         ))}
