1f6c29eca474f5e4ca329b95646100f6f9084bcf
Abogeerick <abogeerick@gmail.com>
2023-07-29 09:25:45 +0300

    Installed React-route dependancies

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+// src/App.js
 import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import axios from "axios";
@@ -5,10 +6,54 @@ import BotCollection from "./components/BotCollection";
 import YourBotArmy from "./components/YourBotArmy";
 import BotDetails from "./components/BotDetails";
 
-
 function App() {
+  const [bots, setBots] = useState([]);
+  const [army, setArmy] = useState([]);
 
+  useEffect(() => {
+    // Fetch bot data from the backend and update the state
+    fetchBots();
+  }, []);
 
+  const fetchBots = async () => {
+    try {
+      const response = await axios.get("http://localhost:8001/bots");
+      setBots(response.data);
+    } catch (error) {
+      console.error("Error fetching bot data: ", error);
+    }
+  };
+
+  const enlistBot = (bot) => {
+    // Add the bot to the army state
+    setArmy([...army, bot]);
+  };
+
+  const releaseBot = (botId) => {
+    // Remove the bot from the army state
+    setArmy(army.filter((bot) => bot.id !== botId));
+  };
+
+  const deleteBot = async (botId) => {
+    // Remove the bot from the backend and update the army state
+    try {
+      await axios.delete(`http://localhost:8001/bots/${botId}`);
+      releaseBot(botId);
+    } catch (error) {
+      console.error("Error deleting bot: ", error);
+    }
+  };
+
+  return (
+    <Router>
+      <div>
+        <h1>Bot Battlr</h1>
+        <Route exact path="/" render={() => <BotCollection bots={bots} enlistBot={enlistBot} />} />
+        <Route path="/your-bot-army" render={() => <YourBotArmy army={army} releaseBot={releaseBot} />} />
+        <Route path="/bots/:id" render={(props) => <BotDetails {...props} deleteBot={deleteBot} />} />
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
