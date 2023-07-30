76c480fa3f1388415e5acde1256769e2886ec67e
Abogeerick <abogeerick@gmail.com>
2023-07-30 09:31:50 +0300

    Added the show BotSpecs component that shows bot details when enlist button is clicked

--- src/components/BotCollection.jsx
+++ src/components/BotCollection.jsx
@@ -5,7 +5,7 @@ import SortBar from "./SortBar";
 import FilterBar from "./FilterBar";
 import "../styles.css";
 
-function BotCollection({ bots, enlistBot }) {
+function BotCollection({ bots, enlistBot, filterBotsByClass }) {
   const [selectedBot, setSelectedBot] = useState(null);
   const [filteredBots, setFilteredBots] = useState(bots);
 
@@ -13,9 +13,11 @@ function BotCollection({ bots, enlistBot }) {
     setSelectedBot(bot);
   };
 
-  const handleEnlistBot = (bot) => {
-    enlistBot(bot);
-    setSelectedBot(null);
+  const handleEnlistBot = () => {
+    if (selectedBot) {
+      enlistBot(selectedBot);
+      setSelectedBot(null);
+    }
   };
 
   const handleSort = (field) => {
@@ -38,25 +40,28 @@ function BotCollection({ bots, enlistBot }) {
 
   return (
     <div>
-      <FilterBar onFilter={handleFilterByClass} />
-      <SortBar onSort={handleSort} />
-      <h2>Available Bots</h2>
-      <ul className="bot-grid">
-        {filteredBots.map((bot) => (
-          <li key={bot.id} className="bot-card" onClick={() => handleBotCardClick(bot)}>
-            <Link to={`/bots/${bot.id}`} className="bot-link">
-              <img src={bot.avatar_url} alt={`Avatar of ${bot.name}`} />
-              <div className="bot-info">
-                <div className="bot-name">{bot.name}</div>
-                <div className="bot-type">{bot.type}</div>
-              </div>
-            </Link>
-            <button onClick={() => handleEnlistBot(bot)}>Enlist</button>
-          </li>
-        ))}
-      </ul>
-      {selectedBot && (
-        <BotSpecs bot={selectedBot} enlistBot={() => handleEnlistBot(selectedBot)} />
+      {selectedBot ? (
+        <BotSpecs bot={selectedBot} enlistBot={handleEnlistBot} />
+      ) : (
+        <>
+          <FilterBar onFilter={handleFilterByClass} />
+          <SortBar onSort={handleSort} />
+          <h2>Available Bots</h2>
+          <ul className="bot-grid">
+            {filteredBots.map((bot) => (
+              <li key={bot.id} className="bot-card" onClick={() => handleBotCardClick(bot)}>
+                <Link to={`/bot-specs/${bot.id}`} className="bot-link">
+                  <img src={bot.avatar_url} alt={`Avatar of ${bot.name}`} />
+                  <div className="bot-info">
+                    <div className="bot-name">{bot.name}</div>
+                    <div className="bot-type">{bot.type}</div>
+                  </div>
+                </Link>
+                <button onClick={() => handleEnlistBot(bot)}>Enlist</button>
+              </li>
+            ))}
+          </ul>
+        </>
       )}
     </div>
   );
